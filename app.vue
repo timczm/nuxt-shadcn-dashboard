fe44f2613359316e5d08417c14dd9b2a205aff16
chenzhiming <timczm@foxmail.com>
2025-09-28 16:10:04 +0800

    更新 title, favicon；删除不必要的信息

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@ useHead({
   },
 })
 
-const title = 'Nuxt Shadcn UI - Dashboard Template'
+const title = 'Dashboard Demo'
 const description = 'This dashboard, built with Nuxt, Shadcn UI, and UnoCSS. It includes a dark mode toggle and is optimized for performance and data efficiency.'
 
 useSeoMeta({
@@ -34,12 +34,12 @@ useSeoMeta({
   description,
   ogTitle: title,
   ogDescription: description,
-  ogUrl: 'https://dashboard.dianprata.com',
-  ogImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
+  // ogUrl: 'https://dashboard.dianprata.com',
+  // ogImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
   twitterTitle: title,
   twitterDescription: description,
-  twitterImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
-  twitterCard: 'summary_large_image',
+  // twitterImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
+  // twitterCard: 'summary_large_image',
 })
 
 const router = useRouter()
